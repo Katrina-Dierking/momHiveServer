14576729bc783d9d745c7168d02239472a62adb2
Katrina <kalynndier@gmail.com>
2023-04-17 22:15:01 -0400

    debugging

--- index.js
+++ index.js
@@ -6,3 +6,17 @@ import cors from 'cors'
 const app = express();
 
 app.use(bodyParser.json({limit: '30mb', extended:true}))
+app.use(bodyParser.urlencoded({limit:'30mb', extended:true}))
+app.use(cors())
+
+//mongo
+
+const CONNECTION_URL =
+  "mongodb+srv://momhive:XKX976i9qveLzZHa@cluster0.n9ec0aw.mongodb.net/?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology:true, useFindAndModify:false})
+    .then(()=> app.listen(PORT, () => console.log(`Server running on port: ${PORT}`) ) )
+    .catch((error)=> console.log(error.message)); 
+
+// mongoose.set('useFindAndModify', false)
